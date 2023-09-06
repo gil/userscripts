5fc35d75de8648969fccc19623676d0fcdb1d623
Andre Gil <contato@andregil.org>
2023-09-06 12:07:38 +0200

    slack-larger-emoji-picker.user.js

---
+++ slack-larger-emoji-picker.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Slack: Larger emoji picker
+// @namespace    https://github.com/gil/userscripts
+// @version      0.0.1
+// @description  How am I supposed to pick such small emojis?
+// @author       Andre Gil
+// @match        https://app.slack.com/*
+// @icon         https://www.google.com/s2/favicons?domain=slack.com
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle(`
+  div[aria-label="Emoji picker"] {
+    zoom: 1.5;
+  }
+`);
