be047bd22b53dbdae244b18d684e49f0ff38693f
André Gil <contato@andregil.org>
2024-07-13 02:08:01 +0200

    Update hemeroteca.user.js

--- hemeroteca.user.js
+++ hemeroteca.user.js
@@ -3,12 +3,12 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://memoria.bn.gov.br/*
 // @grant       none
-// @version     0.1
+// @version     0.2
 // @author      -
 // @description 13/07/2024, 21:01:00
 // ==/UserScript==
 
-const VERSION = 0.1;
+const VERSION = 0.2;
 
 window.addEventListener('load', () => {
   document.oncontextmenu = function(){};
