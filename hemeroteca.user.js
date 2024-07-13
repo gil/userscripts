a417c27fba0c87acac863972c03cd62891e29a2d
André Gil <contato@andregil.org>
2024-07-13 02:02:11 +0200

    Create hemeroteca.user.js

---
+++ hemeroteca.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Hemeroteca
+// @namespace   Violentmonkey Scripts
+// @match       https://acervo.folha.com.br/*
+// @grant       none
+// @version     0.1
+// @author      -
+// @description 13/07/2024, 21:01:00
+// ==/UserScript==
+
+const VERSION = 0.1;
+
+window.addEventListener('load', () => {
+  document.oncontextmenu = function(){};
+});
